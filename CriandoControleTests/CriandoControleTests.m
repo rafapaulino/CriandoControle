3b8dc81ceba6929f75b69125c8c9696890d630c2
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-01 11:24:58 -0300

    Initial Commit

---
+++ CriandoControleTests/CriandoControleTests.m
@@ -0,0 +1,32 @@
+//
+//  CriandoControleTests.m
+//  CriandoControleTests
+//
+//  Created by Rafael Brigagão Paulino on 01/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "CriandoControleTests.h"
+
+@implementation CriandoControleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CriandoControleTests");
+}
+
+@end
